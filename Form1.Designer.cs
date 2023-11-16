57a26fdb3f10c669805d576bebabdda942c61dbc
Scott Nixon <tsnixon@sbcglobal.net>
2023-11-16 11:51:44 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PigLatinGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbEnglish = new System.Windows.Forms.TextBox();
+            this.tbPigLatin = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbEnglish
+            // 
+            this.tbEnglish.Location = new System.Drawing.Point(12, 12);
+            this.tbEnglish.Multiline = true;
+            this.tbEnglish.Name = "tbEnglish";
+            this.tbEnglish.Size = new System.Drawing.Size(358, 67);
+            this.tbEnglish.TabIndex = 0;
+            // 
+            // tbPigLatin
+            // 
+            this.tbPigLatin.Location = new System.Drawing.Point(12, 114);
+            this.tbPigLatin.Multiline = true;
+            this.tbPigLatin.Name = "tbPigLatin";
+            this.tbPigLatin.ReadOnly = true;
+            this.tbPigLatin.Size = new System.Drawing.Size(358, 67);
+            this.tbPigLatin.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(154, 85);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 197);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.tbPigLatin);
+            this.Controls.Add(this.tbEnglish);
+            this.Name = "Form1";
+            this.Text = "Pig Latin Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbEnglish;
+        private System.Windows.Forms.TextBox tbPigLatin;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigLatinGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string[] input = tbEnglish.Text.Split(' ');
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                switch(input[i])
+                {
+                    case ".": case ",": case "?": case ":": case ";": case "-": case "_": case "0":
+                    case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8":
+                    case "9": case "@": case "#": case "$": case "%": case "^": case "&": case "*":
+                        break;
+
+                    default:
+                        input[i] = GetPigLatinWord(input[i]);
+                        break;
+                }
+            }
+
+            if (input.Length > 0)
+                tbPigLatin.Text = String.Join(" ", input);
+            else
+                if (tbEnglish.Text.Length > 0)
+                MessageBox.Show("Unable to generate Pig Latin from your text.");
+        }
+
+
+        public string GetPunctuation(string input)
+        {
+            string puntuationMarks = ".,?!";
+            string mark = "";
+
+            foreach (char c in puntuationMarks.ToCharArray())
+                if (input.Contains(c))
+                    mark = c.ToString();
+
+            return mark;
+        }
+
+
+        public string GetPigLatinWord(string input)
+        {
+            char[] inputChars = input.ToCharArray();
+            string capitalLetter = "";
+            string firstPart = "";
+            string punctuationMark = GetPunctuation(input);
+            string pigLatingString = "";
+
+            if (inputChars[0] >= 0x65 || inputChars[0] <= 0x90)
+                capitalLetter = new string(inputChars, 0, 1);
+
+            if (punctuationMark.Length > 0)
+                input = input.Replace(punctuationMark, "");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input.Substring(i, 1))
+                {
+                    case "A": case "E": case "I": case "O": case "U":
+                        if (i == 0)
+                            pigLatingString = input + "yay";
+                        else
+                            pigLatingString = input.Substring(i) + firstPart + "ay";
+
+                        i = input.Length;
+
+                        break;
+
+                    case "a": case "e": case "i": case "o": case "u":
+                        if (i == 0)
+                            pigLatingString = input + "yay";
+                        else
+                        {
+                            if (capitalLetter.Length > 0) 
+                            {
+                                capitalLetter = input.Substring(i, 1).ToUpper();
+
+                                if (i == input.Length - 1)
+                                    pigLatingString = capitalLetter + firstPart + "ay";
+                                else
+                                    pigLatingString = capitalLetter + input.Substring(i + 1) + firstPart + "ay";
+                            }
+                            else
+                            {
+                                if (i == input.Length - 1)
+                                    pigLatingString = input.Substring(i) + firstPart + "ay";
+                                else
+                                    pigLatingString = input.Substring(i + 1) + firstPart + "ay";
+                            }
+                        }
+
+                        i = input.Length;
+
+                        break;
+
+                    default:
+                        if (input.Substring(i, 1) == "y")
+                        {
+                            if (i == input.Length - 1)
+                            {
+                                if (capitalLetter.Length > 0)
+                                    pigLatingString = "I" + firstPart + "ay";
+                                else
+                                    pigLatingString = "i" + firstPart + "ay";
+                            
+                                i = input.Length;
+                            }
+                        }
+                        else
+                            firstPart += input.Substring(i, 1).ToLower();
+
+                        break;
+                }
+            }
+
+            return pigLatingString + punctuationMark;
+        }
+    }
+}
